5c2e06ec73e17d01c0db009a7d2de5bc16d6a105
blanccobb <121212112@naver.com>
2019-04-14 19:32:43 +0900

    add cmd and matchers

--- chapter02/sample/cmd/main.go
+++ chapter02/sample/cmd/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"log"
 	"os"
+
+	_ "github.com/blanccobb/goinaction/chapter02/sample/matchers"
+	"github.com/blanccobb/goinaction/chapter02/sample/search"
 )
 
 // init()는 main()보다 먼저 호출됨.
 func init() {
 	// change log output to stdout
 	log.SetOutput(os.Stdout)
+}
+
+func main() {
+	search.Run("Sherlock Holmes")
 }
